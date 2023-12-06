06dd18baadd2ee185d6b5bb42dc1ccb92da07cad
liuchengyuan <liuchengyuan@codemao.cn>
2023-12-06 11:13:24 +0800

    feat: 增加测试用例

--- __test__/base/func.cpp
+++ __test__/base/func.cpp
@@ -94,5 +94,101 @@ namespace base_func
         utils_struct::Student s2 = getNewObj(s1);
         log << " 返回值的地址 ==>  " << &s2 << log.endl(__LINE__);
     };
+
+
+
+    void modifyObj(utils_struct::Student* old)
+    {
+        old -> age = 13;
+        log << " 传参数之后的地址：  " << old << log.endl(__LINE__);
+    };
+
+    TEST(TEST_base_base_func, func_args_pointer)
+    {
+        log << "【指针传参】 " << log.endl(__LINE__);
+        utils_struct::Student s1(12, "aaa");
+        log << " 传参数之前的地址 ==>  " << &s1 << log.endl(__LINE__);
+        log << " s1.age " << s1.age << log.endl(__LINE__);
+        modifyObj(&s1);
+        log << " s1.age " << s1.age << log.endl(__LINE__);
+    };
+
+    utils_struct::Student* getNewObjPointer(utils_struct::Student* old)
+    {
+        old -> age = 13;
+        log << " 传参数之后的地址：  " << old << log.endl(__LINE__);
+        return old;
+    };
+
+
+    /**
+     * 指针作为参数，返回传入的指针
+    */
+    TEST(TEST_base_base_func, func_return_pointer)
+    {
+        log << "【参数是指针，返回指针】 " << log.endl(__LINE__);
+        utils_struct::Student s1(12, "aaa");
+
+        log << " 传参数之前的地址 ==>  " << &s1 << log.endl(__LINE__);
+        log << " s1.age " << s1.age << log.endl(__LINE__);
+        utils_struct::Student* s2 = getNewObjPointer(&s1);
+        log << " s2->age " << s2->age << log.endl(__LINE__);
+        log << " 返回的指针 " << s2 << log.endl(__LINE__);
+    };
+
+
+    // 错误示例
+    // utils_struct::Student* getNewObjPointer(utils_struct::Student old)
+    // {
+    //     old.age = 13;
+    //     log << " 传参数之后的地址：  " << old << log.endl(__LINE__);
+    //     return &old;
+    // };
+
+
+    void modifyObjByRef(utils_struct::Student& old)
+    {
+        old.age = 13;
+        log << " 传参数之后的地址：  " << &old << log.endl(__LINE__);
+    };
+
+    TEST(TEST_base_base_func, func_args_ref)
+    {
+        log << "【参数是引用】 " << log.endl(__LINE__);
+        utils_struct::Student s1(12, "aaa");
+
+        log << " 传参数之前的地址 ==>  " << &s1 << log.endl(__LINE__);
+        log << " s1.age " << s1.age << log.endl(__LINE__);
+        modifyObjByRef(s1);
+        log << " s1.age " << s1.age << log.endl(__LINE__);
+    };
+
+    /**
+     * 错误用法
+    */
+    // utils_struct::Student& getNewObjRef(utils_struct::Student old)
+    // {
+    //     old.age = 13;
+    //     log << " 传参数之后的地址：  " << old << log.endl(__LINE__);
+    //     return old;
+    // };
+    utils_struct::Student& getNewObjRef(utils_struct::Student& old)
+    {
+        old.age = 13;
+        log << " 传参数之后的地址：  " << &old << log.endl(__LINE__);
+        return old;
+    };
+
+
+    TEST(TEST_base_base_func, func_return_ref)
+    {
+        log << "【返回是引用】 " << log.endl(__LINE__);
+        utils_struct::Student s1(12, "aaa");
+
+        log << " 传参数之前的地址 ==>  " << &s1 << log.endl(__LINE__);
+        log << " s1.age " << s1.age << log.endl(__LINE__);
+        utils_struct::Student& s2 = getNewObjRef(s1);
+        log << " 返回的地址 " << &s2 << log.endl(__LINE__);
+    };
 }
 #endif // TEST_base_func
